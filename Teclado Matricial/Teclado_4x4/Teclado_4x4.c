ab52e5b67633bd46febcc321f9290735209036cd
Nahuel <nahuluciano@gmail.com>
2025-06-14 11:50:38 -0300

    Teclado 4x4

--- Teclado Matricial/Teclado_4x4/Teclado_4x4.c
+++ Teclado Matricial/Teclado_4x4/Teclado_4x4.c
@@ -3,17 +3,17 @@
 #include "hardware/gpio.h"
 
 #define ROWS 4
-#define COLS 3
+#define COLS 4
 
 // Configuración de pines (ajusta según tu conexión real)
 const uint row_pins[ROWS] = {2, 3, 4, 5};  // Fila 1 a 4
-const uint col_pins[COLS] = {6, 7, 8};     // Columna 1 a 4
+const uint col_pins[COLS] = {6, 7, 8, 9};     // Columna 1 a 4
 
 const char keymap[ROWS][COLS] = {
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'},
-    {'*', '0', '#'}
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
 };
 
 void init_keyboard() {
